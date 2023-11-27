84636afb07aa163cb9eb6a50f8e489e83a550374
Jin Park <103798287+jinftw64@users.noreply.github.com>
2023-11-27 14:59:06 -0500

    implement todo class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 console.log('Test message');
+
+class Todo {
+  constructor(title, description, dueDate, priority, notes = null, checklist = null) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+  }
+
+  get title() {
+    return this._title;
+  }
+
+  set title(value) {
+    this._title = value;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  set description(value) {
+    this._description = value;
+  }
+
+  get dueDate() {
+    return this._dueDate;
+  }
+
+  set dueDate(value) {
+    this._dueDate = value;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+
+  set priority(value) {
+    this._priority = value;
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+  set notes(value) {
+    this._notes = value;
+  }
+
+  get checklist() {
+    return this._checklist;
+  }
+
+  set checklist(value) {
+    this._checklist = value;
+  }
+}
+
+let test = new Todo('walk everyday', 'this is a good exercise', 'tomorrow', 'high');
+
+console.log(test);
