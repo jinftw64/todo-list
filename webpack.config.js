003a0c8d14a360fee0b93f4956c1b1aa0615ffc1
Jin Park <103798287+jinftw64@users.noreply.github.com>
2023-11-27 14:01:20 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ToDo List',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist/',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
