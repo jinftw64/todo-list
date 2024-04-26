04e58b068a37cd367f3910974f43fe1cd3e6ee26
jin <103798287+jinftw64@users.noreply.github.com>
2024-04-26 13:17:58 -0400

    implement localStorage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 import dom from "./dom";
+import project from './project';
 import handlers from './handlers';
 import Pubsub from './pubsub';
 
@@ -19,15 +20,20 @@ handlers.listenForClicks();
 Pubsub.on('addProject', dom.showProjects);
 Pubsub.on('addProject', dom.resetAndHideDialog);
 Pubsub.on('addProject', dom.showProjectMain);
+Pubsub.on('addProject', project.updateLocalStorage);
 
 Pubsub.on('editProject', dom.showProjects);
 Pubsub.on('editProject', dom.resetAndHideDialog);
+Pubsub.on('editProject', project.updateLocalStorage);
 
 Pubsub.on('addTodo', dom.resetAndHideDialog);
 Pubsub.on('addTodo', dom.showProjectMain);
+Pubsub.on('addTodo', project.updateLocalStorage);
 
 Pubsub.on('editTodo', dom.resetAndHideDialog);
 Pubsub.on('editTodo', dom.showProjectMain);
+Pubsub.on('editTodo', project.updateLocalStorage);
 
 Pubsub.on('deleteTodo', dom.resetAndHideDialog);
 Pubsub.on('deleteTodo', dom.showProjectMain);
+Pubsub.on('deleteTodo', project.updateLocalStorage);
--- src/project.js
+++ src/project.js
@@ -1,5 +1,14 @@
 const project = (() => {
-  const projectList = [];
+  const hasLocalStorage = storageAvailable('localStorage');
+
+  let projectList;
+
+  if (hasLocalStorage) {
+    projectList = localStorage.getItem('projectList') || '[]';
+    projectList = JSON.parse(projectList);
+  } else {
+    projectList = [];
+  }
 
   class Project {
     constructor(title) {
@@ -23,11 +32,45 @@ const project = (() => {
     }
   }
 
+  function updateLocalStorage() {
+    if (hasLocalStorage) {
+      localStorage.setItem('projectList', JSON.stringify(project.projectList));
+    }
+  }
+
+  function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        // everything except Firefox
+        (e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === "QuotaExceededError" ||
+          // Firefox
+          e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
+
   return {
     projectList,
     add,
     edit,
     remove,
+    updateLocalStorage,
   };
 })();
 
